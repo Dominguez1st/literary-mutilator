28be2e8c85237d880bb6e3e174adc02f8392aa69
Robert Dominguez <dominguez.1st@gmail.com>
2019-02-24 21:05:59 -0700

    Forgot to commit all weekend, Finished all of tier 1 and did two of tier 2.

---
+++ script.js
@@ -0,0 +1,39 @@
+function change(){
+	document.getElementById("para1").style.color = 'lightBlue';
+	document.getElementById("para1").style.fontFamily = 'Impact';
+	document.getElementById("para1").style.backgroundColor = 'lightyellow';
+}function changeBack(){
+	document.getElementById("para1").style.color = 'black';
+	document.getElementById("para1").style.fontFamily = 'times new roman';
+	document.getElementById("para1").style.backgroundColor = 'white';
+}
+
+function shuffle(array) {
+	let temporaryValue;
+	let randomIndex;
+
+	// While there remain elements to shuffle...
+	for (let currentIndex = array.length - 1; currentIndex >= 0; currentIndex--) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+document.addEventListener("keypress", function (event) {
+		if (event.key !== "r"){
+			return
+		}
+
+		let paraElement = document.getElementById("test");
+		let paraArray = paraElement.innerText.split(" ");
+		let randParaArray = shuffle(paraArray);
+		paraElement.innerText = randParaArray.join(" ");
+	});
